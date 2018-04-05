1d8b363b92b86e8ff9c775883efddb47b87e1bf3
Perrone Romain <rperrone@PC107-042-11.insa-lyon.fr>
2018-04-05 08:19:41 +0200

    modifs

--- Pion.java
+++ Pion.java
@@ -5,19 +5,32 @@ import java.io.File;
 import java.io.IOException;
 
 public class Pion extends Piece {
+  private Image img;
 
   public Pion(String c){
     super(c,"Pion",1);
-  }
-
-  public void afficher(Graphics g, int i, int j){
+    
     Image img = null;
-
     try {
       img = ImageIO.read(new File(super.file+super.nom+(this.couleur == "noir" ? "N.png" : "B.png")));
     } catch(IOException e){}
-
-     g.drawImage(img, 15+i*100, 15+j*100, null);
+    
   }
+
+  public void afficher(Graphics g, int i, int j){
+    g.drawImage(img, 15+i*100, 15+j*100, null);
+  }
+  
+  public void afficher(Graphics g, int x, int y, boolean custom){
+	if( !custom ) {
+		afficher(g,x,y);
+	} else {
+		g.drawImage(img, x, y, null);
+	}
+  }
+  
+  public String toString(){
+	 return "Pion "+this.couleur;
+  }  
 
 }
